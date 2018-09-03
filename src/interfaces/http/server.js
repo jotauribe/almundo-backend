ac3cb6fbe8453a39e9f894c1008e023eccfdf9ff
Uribe Echenique <j.uribe.echenique@accenture.com>
2018-09-03 15:16:57 -0500

    initial commit

--- src/interfaces/http/server.js
+++ src/interfaces/http/server.js
@@ -1,36 +1,38 @@
 const Express = require("express");
 const createError = require("http-errors");
-var cors = require("cors");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const hotelRouter = require("./routes");
 
-const server = Express();
+function createServer() {
+  const server = Express();
 
-server.set("view engine", "ejs");
-server.use(logger("dev"));
-server.use(cors());
-server.use(Express.json());
-server.use(Express.urlencoded({ extended: false }));
-server.use(cookieParser());
+  server.set("view engine", "ejs");
+  server.use(logger("dev"));
+  server.use(cors());
+  server.use(Express.json());
+  server.use(Express.urlencoded({ extended: false }));
+  server.use(cookieParser());
 
-// error handler
-server.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // error handler
+  server.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.sendStatus(err.status || 500);
-});
+    // render the error page
+    res.sendStatus(err.status || 500);
+  });
 
-// endpoints
-server.use("/api", hotelRouter);
+  // endpoints
+  server.use("/api", hotelRouter);
 
-// catch 404 and forward to error handler
-server.use(function(req, res, next) {
-  if (req.app.get("env") === "development") return next(createError(404));
-  next();
-});
+  // catch 404 and forward to error handler
+  server.use(function(req, res, next) {
+    if (req.app.get("env") === "development") return next(createError(404));
+    next();
+  });
+}
 
 module.exports = server;
