25dd1eb694f8cf0f892e0953c173c3067c738b26
Uribe Echenique <j.uribe.echenique@accenture.com>
2018-09-07 11:50:22 -0500

    ramdon commit

--- src/interfaces/http/server.js
+++ src/interfaces/http/server.js
@@ -1,15 +1,15 @@
-const Express = require("express");
-const createError = require("http-errors");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const hotelRouter = require("./routes");
+const Express = require('express');
+const createError = require('http-errors');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hotelRouter = require('./routes');
 
 function createServer() {
   const server = Express();
 
-  server.set("view engine", "ejs");
-  server.use(logger("dev"));
+  server.set('view engine', 'ejs');
+  server.use(logger('dev'));
   server.use(cors());
   server.use(Express.json());
   server.use(Express.urlencoded({ extended: false }));
@@ -19,20 +19,22 @@ function createServer() {
   server.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.sendStatus(err.status || 500);
   });
 
   // endpoints
-  server.use("/api", hotelRouter);
+  server.use('/api', hotelRouter);
 
   // catch 404 and forward to error handler
   server.use(function(req, res, next) {
-    if (req.app.get("env") === "development") return next(createError(404));
+    if (req.app.get('env') === 'development') return next(createError(404));
     next();
   });
+
+  return server;
 }
 
-module.exports = server;
+module.exports = createServer();
